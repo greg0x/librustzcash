b3fcb944e5bfada4f08a1a485a203688f0a9c45a
greg0x <greg0x@users.noreply.github.com>
2026-02-10 10:43:27 +0000

    deploy: 4030de2e59ca11c8b1950d2266db548fb0b3a4d4

---
+++ rustdoc/latest/eip681/error/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Erc55ValidationFailureReason","ParseError","ValidationError"]};
---
+++ rustdoc/latest/trait.impl/zcash_primitives/transaction/fees/trait.FeeRule.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["zcash_client_backend",[["impl FeeRule for <a class=\"enum\" href=\"zcash_client_backend/fees/enum.StandardFeeRule.html\" title=\"enum zcash_client_backend::fees::StandardFeeRule\">StandardFeeRule</a>",0]]],["zcash_primitives",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[207,24]}
